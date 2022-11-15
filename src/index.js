6c6b17b6c02eb2b382b2436e9c08e30accb08406
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2022-11-16 01:32:23 +0300

    Provider changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BaseProvider } from './BaseProvider';
+import { BaseContextProvider } from './BaseContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BaseProvider>
+    <BaseContextProvider>
       <App />
-    </BaseProvider>
+    </BaseContextProvider>
   </React.StrictMode>
 );
 
