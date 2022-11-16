399748ddd5d5e4c7721ac45205ea21c97ef7ca02
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2022-11-16 21:25:21 +0300

    Some changes for functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
     setContent,
     content,
     setWords,
-    words
+    words,
+    speed,
+    setSpeed
   } = useContext(BaseContext);
 
   const buttonOnPressed = () => {
@@ -23,7 +25,7 @@ function App() {
     {
       setWords(wordsArray);
       setIsActive(true);
-      console.log(words);
+      setCurrentWordIndex(0);
       setCurrentWord(wordsArray[currentWordIndex]);
     }
   }
@@ -32,31 +34,44 @@ function App() {
     let interval = null;
     if (isActive) {
       interval = setInterval(() => {
-        setCurrentWordIndex(currentWordIndex => currentWordIndex + 1);
-        setCurrentWord(words[currentWordIndex]);
-        console.log("xx", words[currentWordIndex], currentWordIndex);
-      }, 1000);
+        setCurrentWordIndex(currentWordIndex + 1);
+
+        if(typeof words[currentWordIndex] !== "undefined")
+        {
+          setCurrentWord(words[currentWordIndex]);
+        }
+        else {
+          setIsActive(false);
+          clearInterval(0);
+        }
+        
+      }, speed);
     } else if (!isActive && currentWordIndex !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, setCurrentWordIndex]);
+  }, [isActive, currentWordIndex]);
 
 
   if(isActive == false)
   {
     return (
-      <div className="App">
-          <textarea className="content" name="content" onChange={(e) => setContent(e.target.value)} value={content ?? ""}>{content ?? ""}</textarea>
-          <input className='button' value={"Start"} type="button" onClick={buttonOnPressed} />
+      <div className="App h-screen">
+          <textarea className="bg-emerald-300 p-5" name="content" onChange={(e) => setContent(e.target.value)} value={content ?? ""}>{content ?? ""}</textarea>
+          <br />
+          <br/>
+          <input name='speed' className='bg-emerald-300 p-5' value={speed} onChange={(e) => setSpeed(e.target.value)} />
+          <br />
+          <br />
+          <input className='p-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none' value={"Start"} type="button" onClick={buttonOnPressed} />
       </div>
     );
   }
   else {
     return (
-      <>
+      <div className='bg-slate-900 grid place-items-center h-screen text-pink-500 text-3xl	'>
       {currentWord}
-      </>
+      </div>
     );
   }
 
